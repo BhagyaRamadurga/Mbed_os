2d861dd975feba999ed926e02a9ce077f1b4491c
BhagyaRamadurga <bhagyani2941@gmail.com>
2026-03-05 14:18:08 +0530

    blink led

---
+++ BUILD/DISCO_L496AG/ARMC6/mbed-os/hal/source/mbed_itm_api.d
@@ -0,0 +1,3 @@
+BUILD/DISCO_L496AG/ARMC6/mbed-os/hal/source/mbed_itm_api.o: \
+  mbed-os/hal/source/mbed_itm_api.c \
+  BUILD/DISCO_L496AG/ARMC6/mbed_config.h
---
+++ BUILD/DISCO_L496AG/ARMC6/mbed-os/platform/source/newlib_nano_malloc_workaround.d
@@ -0,0 +1,3 @@
+BUILD/DISCO_L496AG/ARMC6/mbed-os/platform/source/newlib_nano_malloc_workaround.o: \
+  mbed-os/platform/source/newlib_nano_malloc_workaround.c \
+  BUILD/DISCO_L496AG/ARMC6/mbed_config.h
