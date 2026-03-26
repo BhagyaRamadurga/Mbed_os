fb8d961c7673b260c3cc03d21ff258e62f817cc4
BhagyaRamadurga <bhagyani2941@gmail.com>
2026-03-26 12:41:08 +0530

    Refactor LED control logic and improve button handling in main loop

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
+#define BLINKING_RATE    500ms
+#define BUTTON_POLL_RATE 50ms
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
+#ifdef LED1
+void set_led1_state(DigitalOut &led, bool on)
+{
+    led = on ? 1 : 0;
+}
+#endif
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
 #ifdef LED1
     DigitalOut led(LED1);
 #else
-    bool led;
+    bool led = false;
+#endif
+
+#ifdef BUTTON1
+    // BUTTON1 is often active-low
+    DigitalIn button(BUTTON1, PullUp);
 #endif
 
     while (true) {
-        led = !led;
+#if defined(LED1) && defined(BUTTON1)
+        bool pressed = (button.read() == 0);
+        set_led1_state(led, pressed);   // LED ON when button pressed
+        ThisThread::sleep_for(BUTTON_POLL_RATE);
+#elif defined(LED1)
+        led = !led;                     // fallback blink if no button
         ThisThread::sleep_for(BLINKING_RATE);
+#else
+        ThisThread::sleep_for(BUTTON_POLL_RATE);
+#endif
     }
 }
