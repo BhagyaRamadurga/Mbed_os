a90a3470a792992481b6f31ae945514f43b6b922
BhagyaRamadurga <bhagyani2941@gmail.com>
2026-03-26 11:09:19 +0530

    Refactor LED control logic and improve button handling in main loop

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
+#define BUTTON_POLL_RATE  50ms
+
+#ifdef LED1
+void set_led1_state(DigitalOut &led, bool on)
+{
+    led = on ? 1 : 0;
+}
+#endif
 
 
 int main()
@@ -16,11 +24,24 @@ int main()
 #ifdef LED1
     DigitalOut led(LED1);
 #else
-    bool led;
+    bool led = false;
+#endif
+
+#ifdef BUTTON1
+    // Many boards wire BUTTON1 as active-low, so PullUp is used.
+    DigitalIn button(BUTTON1, PullUp);
 #endif
 
     while (true) {
+#if defined(LED1) && defined(BUTTON1)
+        bool pressed = (button.read() == 0);
+        set_led1_state(led, pressed);
+        ThisThread::sleep_for(BUTTON_POLL_RATE);
+#elif defined(LED1)
         led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
+#else
+        ThisThread::sleep_for(BUTTON_POLL_RATE);
+#endif
     }
 }
