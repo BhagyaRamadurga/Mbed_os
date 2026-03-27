35c63e4b4f8182fc36d142f9b51acd641d55306c
BhagyaRamadurga <bhagyani2941@gmail.com>
2026-03-27 14:40:37 +0530

    Add conditional LED blinking in main loop

--- main.cpp
+++ main.cpp
@@ -37,6 +37,8 @@ int main()
 #else
         ThisThread::sleep_for(BUTTON_POLL_RATE);
 #endif
-        
+        if led {
+            blink_led1(led);              // blink LED if it's on
+        }
     }
 }
